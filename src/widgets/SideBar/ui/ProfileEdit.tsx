23b8b70f565e7aa4b0161d7e5053b9c6c0fba621
zerokqx <swiftkey382@gmail.com>
2025-10-30 23:56:23 +0300

    feat(New refresh token system)

--- src/widgets/SideBar/ui/ProfileEdit.tsx
+++ src/widgets/SideBar/ui/ProfileEdit.tsx
@@ -1,21 +1,30 @@
 import { ProfileDataDisplay } from '@/entities/user';
 import { CustomMantineButton } from '@/shared/ui/Button';
-import { Space } from '@mantine/core';
+import { Center, Space } from '@mantine/core';
 import { SquarePen } from 'lucide-react';
 import { sidebarTab } from '../model/tab';
+import { motion } from 'motion/react';
 
 export const ProfileEdit = () => {
   const [Taber] = sidebarTab;
+  const MotionButton = motion.create(CustomMantineButton);
   return (
     <Taber.Panel value="profile">
       <ProfileDataDisplay />
 
       <Space h={'1rem'} />
-      <CustomMantineButton w={'100%'} bdrs="xl" variant="gradient">
-        <SquarePen />
-        <Space w={'1rem'} />
-        Изменить профиль
-      </CustomMantineButton>
+      <Center>
+        <MotionButton
+          initial={{ width: '100%' }}
+          whileHover={{ width: '90%' }}
+          bdrs="xl"
+          variant="light"
+        >
+          <SquarePen />
+          <Space w={'1rem'} />
+          Изменить профиль
+        </MotionButton>
+      </Center>
     </Taber.Panel>
   );
 };
