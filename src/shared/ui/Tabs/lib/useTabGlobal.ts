ba090ac0c7029c5892604dd685b45eac6d51084f
zerokqx <swiftkey382@gmail.com>
2025-11-12 19:13:36 +0300

    sync

--- src/shared/ui/Tabs/lib/useTabGlobal.ts
+++ src/shared/ui/Tabs/lib/useTabGlobal.ts
@@ -1,6 +1,7 @@
 import { use } from 'react';
 import { TaberGlobalContext, TaberLocalContext } from '../model/context';
 import type { CreateTabStore } from '../types';
+import { functions, has } from 'lodash';
 
 declare global {
   export type TabsSources = 'sidebar' | 'asside';
@@ -15,7 +16,6 @@ declare global {
     asside: 'chats' | 'search' | 'user' | 'profile';
   }
 }
-
 export const useTabGlobal = <T extends TabsSources, R extends TabsWindows[T]>(
   source: T
 ): CreateTabStore<R> => {
@@ -26,3 +26,18 @@ export const useTabGlobal = <T extends TabsSources, R extends TabsWindows[T]>(
   if (!context) throw new Error('Not find Taber Context');
   return context[source];
 };
+
+export function useTab<T extends TabsSources, R extends TabsWindows[T]>(
+  sorce: T
+): CreateTabStore<R> | null {
+  const context = use(TaberGlobalContext) as Record<
+    TabsSources,
+    CreateTabStore<R>
+  > | null;
+  if (!context) throw new Error('Called in not context');
+
+  if (has(context, sorce)) {
+    return context[sorce];
+  }
+  return null;
+}
