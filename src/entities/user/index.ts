0e9c85c3c9c08c892a2dd28e5a865b8d1baa0ad8
zerokqx <swtiftkey382@gmail.com>
2026-01-18 15:09:24 +0300

    Skeletons aside
    1) Skeletons events in aside bus
    2) Abort getuserById in contacts list

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,6 +1,11 @@
 export * from './lib';
 export * from './model';
 export * from './types';
-export * from './ui';
+export {
+  CurrentUserProfile,
+  ProfileSearchUser,
+  SkeletonProfile,
+  DisplayPermissionSettings,
+} from './ui';
 export { mockUser } from './model/mockUser.ts';
 export { useIsThatMe } from './model/useIsThatMe.ts';
--- src/features/getUserById/api/useUserById.ts
+++ src/features/getUserById/api/useUserById.ts
@@ -1,11 +1,22 @@
 import { $api } from '@/shared/api/repository/$api';
-import { useState } from 'react';
+import { usePrevious } from '@mantine/hooks';
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect, useRef, useState } from 'react';
+
+const useAbortGetUserById = (id: string) => {
+  const client = useQueryClient();
+  return () => client.cancelQueries({ queryKey: ['get', id] });
+};
 
 export const useGetUserById = () => {
   const [id, setId] = useState('');
+  const previous = usePrevious(id);
+  const abortPrevious = useAbortGetUserById(previous ?? '');
+
   const query = $api.jwtProfile.query.useQuery(
     'get',
     '/{user_id}',
+
     {
       params: {
         path: {
@@ -15,5 +26,5 @@ export const useGetUserById = () => {
     },
     { enabled: !!id, select: (data) => data.data }
   );
-  return { ...query, setId, id };
+  return { ...query, setId, id, abortPrevious };
 };
