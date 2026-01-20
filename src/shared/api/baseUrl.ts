4c3b04948288db29670d4914b2c7275a76ed6394
zerokqx <swtiftkey382@gmail.com>
2026-01-20 16:20:29 +0300

    fix

--- src/shared/api/baseUrl.ts
+++ src/shared/api/baseUrl.ts
@@ -1,6 +1,7 @@
 export type Services = 'auth' | 'user' | 'profile' | 'feed' | 'chat';
 export type Version = 'v1';
 export type BaseUrlDomain = `https://${string}`;
+const apiUrl = import.meta.env.VITE_API_URL as string;
 /**
  * @param service
  * @param version Version api
@@ -10,21 +11,19 @@ export type BaseUrlDomain = `https://${string}`;
  * @summary Function for template endpoint
  *
  */
-export function createBaseUrl<S extends Services>(
-  service: S
-): `https://api.yobble.org/v1/${S}`;
+export function createBaseUrl<S extends Services>(service: S): `${string}${S}`;
 export function createBaseUrl<U extends BaseUrlDomain, S extends Services>(
   service: S,
   version: Version,
   url: U
 ): `${U}/${Version}/${S}`;
 export function createBaseUrl<
-  U extends BaseUrlDomain = 'https://api.yobble.org',
+  U extends string  ,
   S extends Services = Services,
 >(
   service: S,
   version: Version = 'v1',
-  url: U = 'https://api.yobble.org' as U
+  url: U = apiUrl as U
 ): `${U}/${Version}/${S}` {
   return `${url}/${version}/${service}`;
 }
