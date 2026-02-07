96fe00fa186316ae6e5080bf8f2cf4c255079afa
zerokqx <swtiftkey382@gmail.com>
2026-02-07 16:34:51 +0300

    aside user refetch

--- src/features/contact/ui/contact-menu.tsx
+++ src/features/contact/ui/contact-menu.tsx
@@ -3,31 +3,45 @@ import { Ellipsis, Plus } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import { useContactAdd } from '../api';
 import type { ReactNode } from 'react';
+import type { Fn } from '@/shared/types/utils/functions';
 
 interface ContactMenu {
   userId: string;
+  onUpdate: (userId: string) => void;
 }
 
 const loader = (bool: boolean, icon: ReactNode) =>
   bool ? <Loader size={16} /> : icon;
 
-export const ContactMenu = ({ userId }: ContactMenu) => {
+const userIdGuard = (callback: Fn, userId: string | undefined | null) => {
+  if (userId) {
+    callback(userId);
+  }
+};
+export const ContactMenu = ({ userId, onUpdate }: ContactMenu) => {
   const { mutate: contactAdd, isPending } = useContactAdd();
   const [t] = useTranslation('contact-menu');
   return (
     <Menu zIndex={1000000} trigger="click">
       <Menu.Target>
         <ActionIcon>
-          <Ellipsis  />
+          <Ellipsis />
         </ActionIcon>
       </Menu.Target>
       <Menu.Dropdown>
         <Menu.Item
           leftSection={loader(isPending, <Plus />)}
           onClick={() => {
-            contactAdd({
-              body: { user_id: userId },
-            });
+            contactAdd(
+              {
+                body: { user_id: userId },
+              },
+              {
+                onSettled(_data, _error, variables) {
+                  userIdGuard(onUpdate, variables.body.user_id);
+                },
+              }
+            );
           }}
         >
           {t('contact-add')}
--- src/widgets/aside/ui/aside.tsx
+++ src/widgets/aside/ui/aside.tsx
@@ -4,6 +4,7 @@ import { lazy, Suspense, useEffect } from 'react';
 import { SkeletonProfile, useGetUserById } from '@/entities/user';
 import { useGetUuidFromRouter } from '@/shared/lib/use-get-uuid-from-router';
 import { useLayoutStore } from '@/shared/lib/hooks/use-layout';
+import { ContactMenu } from '@/features/contact';
 
 const ProfileForGetUserById = lazy(() =>
   import('@/entities/user').then((m) => ({
@@ -18,7 +19,7 @@ export const Aside = ({ onClose }: CustomAsideProps) => {
   const uuid = useGetUuidFromRouter();
 
   const update = useLayoutStore((s) => s.update);
-  const { setId, data, isFetching, abortPrevious } = useGetUserById();
+  const { setId, data, isFetching, abortPrevious, refetch } = useGetUserById();
 
   useEffect(() => {
     if (!uuid) return;
@@ -30,18 +31,28 @@ export const Aside = ({ onClose }: CustomAsideProps) => {
 
   return (
     <AppShellAside zIndex={1000000} style={{ overflow: 'clip' }}>
-      <Group justify="space-between">
-        <Box>
-          <CloseButton onClick={onClose} />
-        </Box>
-      </Group>
-      <Suspense fallback={fallback}>
-        {data ? (
-          <ProfileForGetUserById profile={data} />
-        ) : (
-          isFetching && fallback
-        )}
-      </Suspense>
+      {uuid && (
+        <>
+          <Group justify="space-between">
+            <Box>
+              <CloseButton onClick={onClose} />
+              <ContactMenu
+                onUpdate={() => {
+                  void refetch();
+                }}
+                userId={uuid}
+              />
+            </Box>
+          </Group>
+          <Suspense fallback={fallback}>
+            {data ? (
+              <ProfileForGetUserById profile={data} />
+            ) : (
+              isFetching && fallback
+            )}
+          </Suspense>
+        </>
+      )}
     </AppShellAside>
   );
 };
