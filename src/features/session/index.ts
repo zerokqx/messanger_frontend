87619baee2e4fccbea3c512475055e53166d7216
zerokqx <swtiftkey382@gmail.com>
2026-02-15 22:30:02 +0300

    Limit session public API to slice exports

--- src/features/session/index.ts
+++ src/features/session/index.ts
@@ -1,2 +1,6 @@
-export * from './model';
-export * from './ui';
+export { useRevokeSession, useSessionRevokeAll } from './model';
+export { SessionManage } from './ui';
+export { ModalRevoke } from './ui';
+export type { ModalRevokeProps } from './ui';
+export { RevokeAllModal } from './ui';
+export type { RevokeAllModalProps } from './ui';
